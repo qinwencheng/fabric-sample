cce22a31d1b70613945c854efde16e7429b92459
qinwencheng <157475034@qq.com>
2023-02-26 01:03:22 +0800

    build: 配置组件打包相关参数

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import path from 'path'
+import path, { resolve } from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
@@ -55,5 +55,26 @@ export default defineConfig({
   // https://github.com/vitest-dev/vitest
   test: {
     environment: 'jsdom',
+  },
+
+  build: {
+    minify: false,
+    sourcemap: 'inline',
+    lib: {
+      entry: resolve(__dirname, 'src/components/image-annotation/index.ts'),
+      name: 'ImageAnnotation',
+      fileName: 'image-annotation',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
 })
